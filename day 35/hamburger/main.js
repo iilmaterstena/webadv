f45827c6db2d43866cd987f6ea7723c444d18a68
iilmaterstena <ilmaterstena860@gmail.com>
2025-10-04 14:48:59 +0200

    day 36

--- day 35/hamburger/main.js
+++ day 35/hamburger/main.js
@@ -1,19 +1,25 @@
+function openMenu() {
+  var menu = document.getElementById("myLinks");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
 
-
-function search(){
-    var input, filter, ul, li, a, i, txtvalu;
-    input=document.getElementById("myInput");
+function search() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     ul = document.getElementById("myList");
-    li=ul.getElementById("li");
-
-    for(i=0; i<li.lemgth; i++){
-       a = li[i];
-       txtvalu = a.textContent || a.innerText;
-       if(txtvalu.toUpperCase().indexOf(filter)>-1){
-        li[i].style.display = "block";
-       }else{
-         li[i].style.display = "none";
-       }
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i];
+        txtValue = a.textContent;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
     }
 }
